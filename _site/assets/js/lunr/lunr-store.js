1fc65af59a6dd7632b88782d018edfed902c4d29
Patrik Wigur Martin <patrik.martin@mideye.com>
2023-12-15 12:00:19 +0100

    add logo

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "1691, den 5 januari, Kemi Lappmark",
-        "excerpt":"Kemi Lappmark Den 5 januari 1691 hölls lagting i Kemi lappmark och Kittilä marknadsplats, närvarande kronans befallningsman välborne Jacob Graape med följande nämndemän: Olof Johansson Nils Andersson Nils Nilsson Olof Nilsson Nils Olofsson Pål Olofsson Samma dag publicerades kungens nådigaste barnamordsförordning, daterad den 15 november 1684. Detsamma gäller kungens nådigaste...","categories": [],
-        "tags": [],
-        "url": "/Kittil%C3%A4-Marknadsplats/",
-        "teaser": null
-      }]
